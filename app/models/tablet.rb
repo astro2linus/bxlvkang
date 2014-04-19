86ba18a31f3aaed60bf0953cc31b88846692b386
astro2linus <astro2linus@gmail.com>
2014-04-19 15:07:38 +0800

    display tablet dynamically

--- app/models/tablet.rb
+++ app/models/tablet.rb
@@ -1,2 +1,4 @@
 class Tablet < ActiveRecord::Base
+	validates_presence_of :headline, :short_description, :long_description
+	mount_uploader :image, TabletImageUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Bxlvkang::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root "home#index"
+  resources :tablets
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
