4c8f6a8e02fe4843ee5b1c923860890f1e92c1a7
astro2linus <astro2linus@gmail.com>
2014-05-11 21:47:14 +0800

    change ruby back to 2.0.0

--- app/models/home_masterhead.rb
+++ app/models/home_masterhead.rb
@@ -1,4 +1,4 @@
 class HomeMasterhead < ActiveRecord::Base
 	mount_uploader :image, HomeMasterheadUploader
-	validates_presence_of :image, :header_text, :byline_text
+	validates_presence_of :header_text, :byline_text
 end
